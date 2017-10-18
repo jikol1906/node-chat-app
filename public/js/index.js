a2228c9e0a20c7e83d6291a1668cbd0004410188
Boris Grunwald <borisgrunwald@gmail.com>
2017-10-18 21:56:28 +0200

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
 socket.on('connect', function()  {
    console.log('Connected to server');
 })
@@ -27,14 +44,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
-
-    /*
-    var li = jQuery('<li></li>');
-
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    jQuery('#messages').append(li);*/
-
+    scrollToBottom();
 })
 
 socket.on('newLocationMessage',function (message) {
@@ -49,6 +59,7 @@ socket.on('newLocationMessage',function (message) {
     })
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
