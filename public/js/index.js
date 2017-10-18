2d166990ee2b012ed859c24b251d96216b5d1e8b
Boris Grunwald <borisgrunwald@gmail.com>
2017-10-18 20:36:33 +0200

    Format timestamps using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -18,28 +18,23 @@ function getDiss() {
 
 socket.on('newMessage', function (message) {
 
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
 
-    if(message.from !== 'Admin') {
-        li.text(`${message.from}: ${message.text} ${getDiss()}`);
-    } else {
-        li.text(`${message.from}: ${message.text}`);
-    }
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 
-    console.log(message);
 })
 
 socket.on('newLocationMessage',function (message) {
 
-    console.log("in newlocatinmes");
-    console.log(message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
 
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from}: ${formattedTime} `);
     a.attr('href',message.url);
     li.append(a);
     jQuery('#messages').append(li);
